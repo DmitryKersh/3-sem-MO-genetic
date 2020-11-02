bda9d22e7427b6cb5f9cca4bc542455258fb442a
DmitryKersh <d.karpukhin@mail.ru>
2020-11-02 14:08:01 +0300

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by dimchik on 02.11.2020.
+//
+
+#include <cmath>
+
+inline double fit_func(double x, double y){
+    return sin(x) * sin(y) * (1 + x*x + y*y);
+}
+
+int main() {
+
+    return 0;
+}
+
